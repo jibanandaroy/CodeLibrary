e0c99c3269ec4b5843339fd2936acd7d09f956d3
Jibon <jibonroy282@gmail.com>
2023-07-10 10:58:15 +0600

    first

---
+++ CountDigit/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int countDigit(int n){
+    int x = (int)(log10(n)+1);
+    cout<<x;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    countDigit(n);
+
+    return 0;
+}
